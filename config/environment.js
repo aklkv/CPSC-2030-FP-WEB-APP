f28ce6190e5184409f96315b64022a4c0b12bd4d
Alexey Kulakov <uni_que@me.com>
2016-07-29 05:22:46 -0700

    updated edpoint url

--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.apiURL = 'http://localhost:3000';
+    ENV.apiURL = 'http://localhost';
   }
 
   if (environment === 'test') {
